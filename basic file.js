086fc123050a3850194f058c05f6fc05049707d8
Mujahid Hossain <opee.home@outlook.com>
2024-05-20 00:58:03 +0600

    Create basic file.js

---
+++ basic file.js
@@ -0,0 +1,8 @@
+<script>
+        // JavaScript to handle the button click
+        const addToCartButton = document.querySelector('.add-to-cart-button');
+        addToCartButton.addEventListener('click', () => {
+            // Add your logic here (e.g., update cart, show confirmation message, etc.)
+            alert('Product added to cart!');
+        });
+    </script>
